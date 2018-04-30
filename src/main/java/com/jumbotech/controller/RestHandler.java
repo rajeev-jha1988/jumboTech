7ee8902789a2b6c1cfd647f211018209f3148858
rajeev.jha <raj.k9732@gmail.com>
2018-04-30 22:15:43 +0530

    added listner service for Poc
    Change-Id: I5f086626c4597e9d17cc70220963bf9fabd20f71
    Signed-off-by: rajeev.jha <raj.k9732@gmail.com>

--- src/main/java/com/jumbotech/controller/RestHandler.java
+++ src/main/java/com/jumbotech/controller/RestHandler.java
@@ -5,6 +5,7 @@ package com.jumbotech.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -18,4 +19,62 @@ public class RestHandler {
 	public String sayHello(@PathVariable("name") String name){
 		return "Hello "+name;
 	}
+	
+	@PostMapping("/rest/listnerService1/{snsTopicMsg}")
+	public String listnerService1(@PathVariable("snsTopicMsg") String snsTopicMsg) {
+		try {
+			Thread.sleep(20);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return snsTopicMsg.concat("listnerService1");
+	}
+	
+	@PostMapping("/rest/listnerService2/{snsTopicMsg}")
+	public String listnerService2(@PathVariable("snsTopicMsg") String snsTopicMsg) {
+		try {
+			Thread.sleep(20);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return snsTopicMsg.concat("listnerService2");
+	}
+	
+	@PostMapping("/rest/listnerService3/{snsTopicMsg}")
+	public String listnerService3(@PathVariable("snsTopicMsg") String snsTopicMsg) {
+		try {
+			Thread.sleep(20);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return snsTopicMsg.concat("listnerService3");
+	}
+	
+	@PostMapping("/rest/listnerService4/{snsTopicMsg}")
+	public String listnerService4(@PathVariable("snsTopicMsg") String snsTopicMsg) {
+		try {
+			Thread.sleep(20);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return snsTopicMsg.concat("listnerService4");
+	}
+	
+	@PostMapping("/rest/listnerService5/{snsTopicMsg}")
+	public String listnerService5(@PathVariable("snsTopicMsg") String snsTopicMsg) {
+		try {
+			Thread.sleep(20);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return snsTopicMsg.concat("listnerService5");
+	}
+	
+	
+	
 }
